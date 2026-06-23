21d30d6263e285284303abe722b2506503fdcf48
Your Name <you@example.com>
2026-06-23 15:16:00 +0300

    Fix port for Railway

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 using MySqlConnector;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 // Add MVC
 builder.Services.AddControllersWithViews();
 
